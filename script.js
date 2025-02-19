9def20d26d23a9398093be27a11c672ee480fb73
Jeffrey Inman <jeffrey@enter-n.com>
2025-02-18 20:58:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function updateSliderColor(slider) {
         let value = slider.value;
-        slider.style.background = `linear-gradient(to right, cyan ${0 - value}%, deeppink ${100 - value}%)`;
-        slider.style.background = `linear-gradient(to right, cyan ${value}%, deeppink 0)`;
+        slider.style.background = linear(to right, cyan ${0 - value}%, deeppink ${100 - value}%)`;
     }
 
     function updateProgress() {
@@ -30,3 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+0 commit comments
+Comments
+0
