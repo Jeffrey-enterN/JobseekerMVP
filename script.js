40287190406166f2c1f50d77bfbec0cc8749a0bd
Jeffrey Inman <jeffrey@enter-n.com>
2025-02-18 21:21:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function updateSliderColor(slider) {
         let value = slider.value;
-        slider.style.background = `linear(aqua ${value - 0}%, deeppink ${100 - value}%)`;
-    }
+slider.style.background = `linear-gradient(to right, cyan ${0 - value}%, deeppink ${100 - value}%)`;    }
 
     function updateProgress() {
         let completed = [...sliders].filter(slider => slider.value !== "50").length;
